7e66862dfce11ba9ad0465d589c32c05cd11d7e2
joao felipe alves <joaofelipealves62@gmail.com>
2023-09-28 21:46:27 -0300

    Separando sentenças do documento doc

--- Aula02.py
+++ Aula02.py
@@ -1,3 +1,10 @@
 from spacy import load #Carrega um modelo!
 
 nlp = load('pt_core_news_sm')
+
+doc = nlp(
+    'Eduardo foi a feira. Comprou dois pasteis.'
+)
+
+for sent in doc.sents: #Separa 'doc' em sentenças, e printa elas
+    print(sent)
